e8edfa25acefc5f493c9fa700ab4e796d6cbaf8f
Juan Leni <lenijuan@gmail.com>
2020-08-05 21:30:41 +0200

    fix decompressLEB128 fuzz target

--- fuzz/decompressLEB128.cpp
+++ fuzz/decompressLEB128.cpp
@@ -14,14 +14,8 @@ using std::size_t;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    // looking at the implementation of `decompressLEB128`, it looks at up to
-    // 10 bytes of input (which is not expected to be NUL-terminated).
-    if (size < 10) {
-        return 0;
-    }
-
     uint64_t output;
-    if (decompressLEB128(data, &output) != 0) {
+    if (decompressLEB128(data, size, &output) != 0) {
         return 1;
     }
 
