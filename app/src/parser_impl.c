dadf45ed84ba805a592c0554549d7ab46055ccfc
Fernando Theirs <fernando@zondax.ch>
2021-11-12 14:15:02 -0300

    parse param depending on method number

--- app/src/parser_impl.c
+++ app/src/parser_impl.c
@@ -271,20 +271,18 @@ parser_error_t _printParam(const parser_tx_t *tx, uint8_t paramIdx,
 
     CborValue itParams = itContainer;
 
-    switch (itContainer.type) {
-        case  CborByteStringType: {
+    switch(tx->method) {
+        case 23: {
+            PARSER_ASSERT_OR_ERROR(itContainer.type == CborByteStringType, parser_unexpected_type)
             address_t tmpAddr;
             MEMZERO(&tmpAddr, sizeof(address_t));
             CHECK_PARSER_ERR(readAddress(&tmpAddr, &itContainer))
             PARSER_ASSERT_OR_ERROR(itContainer.type != CborInvalidType, parser_unexpected_type)
-            //Not every ByteStringType must be interpreted as address. Depends on method number and actor.
             CHECK_PARSER_ERR(_printAddress(&tmpAddr, outVal, outValLen, pageIdx, pageCount));
             break;
         }
-        case CborMapType:
-        case CborArrayType:
-        default: {
-            /// Enter container?
+        case 0: {
+            /// Enter container
             CHECK_CBOR_MAP_ERR(cbor_value_enter_container(&itContainer, &itParams))
             CHECK_APP_CANARY()
             for (uint8_t i = 0; i < paramIdx; ++i) {
@@ -302,7 +300,11 @@ parser_error_t _printParam(const parser_tx_t *tx, uint8_t paramIdx,
             CHECK_APP_CANARY()
             break;
         }
+        default:
+            return parser_unexpected_method;
+            break;
     }
+
     return parser_ok;
 }
 
@@ -366,31 +368,45 @@ __Z_INLINE parser_error_t readMethod(parser_tx_t *tx, CborValue *value) {
         CborParser parser;
         CborValue itParams;
         CHECK_CBOR_MAP_ERR(cbor_parser_init(tx->params, paramsLen, 0, &parser, &itParams))
-
-        switch (itParams.type) {
-            case CborArrayType: {
-                size_t arrayLength = 0;
-                CHECK_CBOR_MAP_ERR(cbor_value_get_array_length(&itParams, &arrayLength))
-                PARSER_ASSERT_OR_ERROR(arrayLength < UINT8_MAX, parser_value_out_of_range)
-                tx->numparams = arrayLength;
+        switch (methodValue) {
+            case 0: {
+                switch (itParams.type) {
+                    case CborArrayType: {
+                        size_t arrayLength = 0;
+                        CHECK_CBOR_MAP_ERR(cbor_value_get_array_length(&itParams, &arrayLength))
+                        PARSER_ASSERT_OR_ERROR(arrayLength < UINT8_MAX, parser_value_out_of_range)
+                        tx->numparams = arrayLength;
+                        break;
+                    }
+                    case CborMapType: {
+                        size_t mapLength = 0;
+                        CHECK_CBOR_MAP_ERR(cbor_value_get_map_length(&itParams, &mapLength))
+                        PARSER_ASSERT_OR_ERROR(mapLength < UINT8_MAX, parser_value_out_of_range)
+                        tx->numparams = mapLength;
+                        break;
+                    }
+                    case CborInvalidType:
+                    default:
+                        return parser_unexpected_type;
+                }
                 break;
             }
-            case CborMapType: {
-                size_t mapLength = 0;
-                CHECK_CBOR_MAP_ERR(cbor_value_get_map_length(&itParams, &mapLength))
-                PARSER_ASSERT_OR_ERROR(mapLength < UINT8_MAX, parser_value_out_of_range)
-                tx->numparams = mapLength;
+            case 23: {
+                switch (itParams.type) {
+                    case CborByteStringType: {
+                        //Only one parameter is expected when ByteStringType is received.
+                        PARSER_ASSERT_OR_ERROR(itParams.remaining == 1, parser_value_out_of_range)
+                        tx->numparams = 1;
+                        break;
+                    }
+                    default:
+                        return parser_unexpected_type;
+                }
                 break;
             }
-            case CborByteStringType: {
-                //Only one parameter is expected when ByteStringType is received.
-                PARSER_ASSERT_OR_ERROR(itParams.remaining == 1, parser_value_out_of_range)
-                tx->numparams = 1;
-                break;
+            default: {
+                return parser_unexpected_method;
             }
-            case CborInvalidType:
-            default:
-                return parser_unexpected_type;
         }
     }
     tx->method = methodValue;
