131883d05f5dc5f0c3c5acb84d0770e779064362
abenso <andresbenso@gmail.com>
2025-08-20 16:05:54 -0300

    add no bls test

--- tests_zemu/tests/eip191_fvm.test.ts
+++ tests_zemu/tests/eip191_fvm.test.ts
@@ -13,12 +13,19 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  ******************************************************************************* */
-import Zemu from "@zondax/zemu";
+import Zemu, {
+  ButtonKind,
+  TouchNavigation,
+  ClickNavigation,
+  isTouchDevice,
+} from "@zondax/zemu";
 // @ts-ignore
 import { FilecoinApp } from "@zondax/ledger-filecoin";
 import { defaultOptions, models, PATH, EIP191_FVM_PREFIX } from "./common";
 import { getBlakeHash } from "./utils";
 import * as secp256k1 from "secp256k1";
+import { getTouchElement } from "@zondax/zemu/dist/buttons";
+import { IButton } from "@zondax/zemu/dist/types";
 
 jest.setTimeout(90000);
 
@@ -48,7 +55,7 @@ const SIGN_TEST_DATA = [
 
 describe.each(models)("EIP191", function (m) {
   test.concurrent.each(SIGN_TEST_DATA)(
-    "sign transaction: $name",
+    `sign transaction: $name for ${m.name}`,
     async function (data) {
       const sim = new Zemu(m.path);
       try {
@@ -107,3 +114,92 @@ describe.each(models)("EIP191", function (m) {
     },
   );
 });
+
+describe.each(models)("EIP191 no bls", function (m) {
+  // only test personal_sign_non_printable_msg
+  test.only.each(SIGN_TEST_DATA.filter(d => d.name === 'personal_sign_non_printable_msg'))(
+    `sign no bls transaction $name for ${m.name}`,
+    async function (data) {
+      const sim = new Zemu(m.path);
+      try {
+        await sim.start({ ...defaultOptions, model: m.name });
+        const app = new FilecoinApp(sim.getTransport());
+
+        // Get public key and address
+        const publicKeyResponse = await app.getAddressAndPubKey(PATH);
+
+        // Start signature request, should fail because blind signing is not enabled
+        const signNonBls = app.signPersonalMessageFVM(PATH, data.message);
+
+        await Zemu.sleep(500);
+  
+        // Wait until we are not in the main menu
+        console.log("STEP 0");
+        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+        console.log("STEP 1");
+
+        let nav = undefined;
+        if (isTouchDevice(m.name)) {
+          const rejectButton: IButton = getTouchElement(m.name, ButtonKind.RejectButton)
+          nav = new TouchNavigation(m.name, [ButtonKind.RejectButton]);
+          nav.schedule[0].button = rejectButton;
+        } else {
+          nav = new ClickNavigation([0]);
+        }
+
+
+        await sim.navigate(
+          ".",
+          `${m.prefix.toLowerCase()}-fvm-no-bls-${data.name}`,
+          nav.schedule,
+        );
+
+        await expect(signNonBls).rejects.toThrow("Data is invalid : Blindsign Mode Required");
+
+        sim.clearTransportError();
+        await sim.toggleBlindSigning();
+
+        const signatureRequest = app.signPersonalMessageFVM(PATH, data.message);
+
+        // Wait until we are not in the main menu and approve
+        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+        await sim.compareSnapshotsAndApprove(
+          ".",
+          `${m.prefix.toLowerCase()}-fvm-no-bls-${data.name}`,
+          true,
+          2,
+          1500,
+          data.blind,
+        );
+
+        const signatureResponse = await signatureRequest;
+
+        // Construct EIP-191 message format: "\x19Filecoin Signed Message:\n" + len + message
+        const msgLengthString = String(data.message.length);
+        const eip191Message = Buffer.concat([
+          EIP191_FVM_PREFIX,
+          Buffer.from(msgLengthString, "utf8"),
+          data.message,
+        ]);
+        const messageHash = getBlakeHash(eip191Message);
+
+        // Verify signature
+        const publicKey = Uint8Array.from(publicKeyResponse.compressed_pk);
+        const signature = secp256k1.signatureImport(
+          Uint8Array.from(signatureResponse.signature_der),
+        );
+
+        const isSignatureValid = secp256k1.ecdsaVerify(
+          signature,
+          Uint8Array.from(messageHash),
+          publicKey,
+        );
+
+        expect(isSignatureValid).toEqual(true);
+      } finally {
+        await sim.close();
+      }
+    },
+  );
+});
+
