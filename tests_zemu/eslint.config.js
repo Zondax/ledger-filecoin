37bed680180281752f0dd922ef9d5f0cf674a5cf
abenso <andresbenso@gmail.com>
2025-08-22 11:37:57 -0300

    fix eslint

--- tests_zemu/eslint.config.js
+++ tests_zemu/eslint.config.js
@@ -1,5 +1,22 @@
 const typescriptParser = require("@typescript-eslint/parser");
 
+const commonRules = {
+  curly: "warn",
+  "prefer-const": "warn",
+  "no-else-return": "warn",
+  complexity: ["warn", 1000],
+  "no-unneeded-ternary": "warn",
+  "no-alert": "warn",
+  "no-empty": "warn",
+  "no-useless-catch": "error",
+  "require-await": "warn",
+  "no-continue": "warn",
+  "no-console": "off",
+  "unused-imports/no-unused-imports": "warn",
+  "no-unused-vars": "warn",
+  "no-magic-numbers": "off",
+};
+
 module.exports = [
   {
     ignores: ["dist/*", "node_modules/*"],
@@ -34,22 +51,7 @@ module.exports = [
       "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
       "eslint-plugin-tsdoc": require("eslint-plugin-tsdoc"),
     },
-    rules: {
-      curly: "warn",
-      "prefer-const": "warn",
-      "no-else-return": "warn",
-      complexity: ["warn", 1000],
-      "no-unneeded-ternary": "warn",
-      "no-alert": "warn",
-      "no-empty": "warn",
-      "no-useless-catch": "error",
-      "require-await": "warn",
-      "no-continue": "warn",
-      "no-console": "off",
-      "unused-imports/no-unused-imports": "warn",
-      "no-unused-vars": "warn",
-      "no-magic-numbers": "off",
-    },
+    rules: commonRules,
   },
   {
     files: ["**/*.js", "**/*.mjs"],
@@ -66,21 +68,6 @@ module.exports = [
     plugins: {
       "unused-imports": require("eslint-plugin-unused-imports"),
     },
-    rules: {
-      curly: "warn",
-      "prefer-const": "warn",
-      "no-else-return": "warn",
-      complexity: ["warn", 1000],
-      "no-unneeded-ternary": "warn",
-      "no-alert": "warn",
-      "no-empty": "warn",
-      "no-useless-catch": "error",
-      "require-await": "warn",
-      "no-continue": "warn",
-      "no-console": "off",
-      "unused-imports/no-unused-imports": "warn",
-      "no-unused-vars": "warn",
-      "no-magic-numbers": "off",
-    },
+    rules: commonRules,
   },
 ];
